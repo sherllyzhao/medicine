2d488cffb36f077ae66c3f640e14e8ed63d544bd
sherllyzhao <1253108690@qq.com>
2024-07-20 21:24:39 +0800

    公共头部

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,18 +1,49 @@
-const Layout = props => {
+import {
+  Breadcrumb,
+  Layout as LayoutWrap,
+  Menu,
+  theme,
+  Avatar,
+  Space,
+} from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { useState } from "react";
+const { Header, Content, Sider } = LayoutWrap;
+
+const Layout = (props) => {
   const { childEle } = props;
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    avatar: "",
+  });
 
   // 图片
-  const logo = require('@/logo.svg');
+  const logo = require("@/assets/images/logo.png");
 
-  return <div className="main">
-    <header className="flex justify-around items-center h-[50px] bg-[rgb(229, 231, 235)]">
-      <div className="left">
-        <img src={logo} alt="" />
-      </div>
-    </header>
-    <div className="container">
-      {childEle}
-    </div>
-  </div>
-}
-export default Layout
+  return (
+    <LayoutWrap>
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div className="flex items-center">
+          <img src={logo} className="w-[40px]" />
+
+          <div className="text-[white] text-[18px] ml-[20px]">中医药系统</div>
+        </div>
+
+        <div className="flex items-center">
+          {userInfo.avatar ? (
+            <Avatar size={64} src={userInfo.avatar} />
+          ) : (
+            <Avatar size={64} icon={<UserOutlined />} />
+          )}
+        </div>
+      </Header>
+    </LayoutWrap>
+  );
+};
+export default Layout;
